bf8ec328525f12f6c14acdcca32faab48e0e7d46
Ella Schneyer <emschneyer@gmail.com>
2026-03-11 21:54:38 -0400

    polished lru, naive lru

--- src/policies/naivelru.rs
+++ src/policies/naivelru.rs
@@ -1,10 +1,8 @@
 use crate::policy::{Policy, CacheKey}; //bringing policy trait into scope
-
 use indexmap::IndexSet;
 
 pub struct LruPolicy {
     order: IndexSet<CacheKey>,//only care about key ordering
-    capacity: usize
 }
 
 impl LruPolicy {
@@ -12,34 +10,28 @@ impl LruPolicy {
     pub fn new(capacity: usize) -> Self {
         Self { //initialize LRU policy
             order: IndexSet::with_capacity(capacity),
-            capacity
         }
     }
 }
 
 impl Policy for LruPolicy { 
-    fn on_hit(&mut self, key: CacheKey){
+    fn on_hit(&mut self, key: CacheKey) {
         // key accessed, moved to the back(more recent)
         self.order.shift_remove(&key);
         self.order.insert(key);
     }
 
-    fn on_miss(&mut self, key: CacheKey) -> Option<CacheKey> {
-        // check if we need to remove an element from cache
-        if(self.order.len() >= self.capacity){
-            // yes, front of list
-            self.order.get_index(0)
-        }else{
-            // still has room
-            None
-        }
-    }
+    fn on_miss(&mut self, key: CacheKey) {}
 
-    fn insert(&mut self, key: CacheKey){
+    fn insert(&mut self, key: CacheKey) {
         self.order.insert(key);
     }
 
-    fn remove(&mut self, key: CacheKey){
+    fn remove(&mut self, key: CacheKey) {
         self.order.shift_remove(&key);
+    }
+
+    fn victim(&mut self) -> Option<CacheKey> {
+        self.order.get_index(0).copied()
     }
 }
