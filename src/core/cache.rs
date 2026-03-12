bf79f93d8664e60d3ef5017c30854a09299b0c96
Ella Schneyer <emschneyer@gmail.com>
2026-03-12 00:20:09 -0400

    polished cache and main mem

--- src/core/cache.rs
+++ src/core/cache.rs
@@ -19,7 +19,7 @@ impl<P: Policy, const MM_SIZE: usize> Cache<P, MM_SIZE> {
     pub fn new(capacity: usize, policy: P, main_memory: MainMemory<MM_SIZE>) -> Self {
         Self {
             capacity,
-            store: HashMap::new(),
+            store: HashMap::with_capacity(capacity),
             policy,
             metrics: Metrics::new(),
             clock: Clock::new(),
@@ -28,38 +28,36 @@ impl<P: Policy, const MM_SIZE: usize> Cache<P, MM_SIZE> {
     }
 
     pub fn access(&mut self, key: CacheKey) {
-        self.clock.tick_up();
-        let tick = self.clock.get_tick();
+        let tick = self.clock.tick();
 
         if let Some(entry) = self.store.get_mut(&key) {
             // Hit
             entry.on_access(tick);
             self.policy.on_hit(key);
             self.metrics.record_hit();
-        } else {
-            // Miss
-            self.metrics.record_miss();
+            return;
+        }
 
-            // If at capacity, ask policy who to evict
+        // Miss
+        self.metrics.record_miss();
+        self.policy.on_miss(key);
 
-            /* on_miss and victim are interchangeable ig? and then remove is the thing that tells the policy that we 
-            did actually get rid of something
-            */
-            if self.store.len() >= self.capacity {
-                if let Some(evict_key) = self.policy.on_miss(key) {
-                    self.store.remove(&evict_key);
-                    self.policy.remove(evict_key);
-                    self.metrics.record_eviction();
-                }
-            } else {
-                self.policy.on_miss(key);
+        if self.capacity == 0 {
+            return;
+        }
+
+        // If at capacity, ask policy who to evict
+        if self.store.len() >= self.capacity {
+            if let Some(evict_key) = self.policy.victim() {
+                self.store.remove(&evict_key);
+                self.policy.remove(evict_key);
+                self.metrics.record_eviction();
             }
+        }
 
-            // Fetch from main memory
-            let _ = self.main_memory.fetch(key);
-
-            // Insert new entry
-            let entry = Entry::new(key, 1, tick);
+        // Fetch from main memory and insert into cache
+        if let Some(obj) = self.main_memory.fetch(&key) {
+            let entry = Entry::new(obj.key, obj.size_in_bytes, tick);
             self.store.insert(key, entry);
             self.policy.insert(key);
         }
