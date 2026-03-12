593171fa4c09e8a736dae4c8a2b3394820b3ebc2
Ella Schneyer <emschneyer@gmail.com>
2026-03-11 20:45:59 -0400

    polished metrics, trace

--- src/core/trace.rs
+++ src/core/trace.rs
@@ -1,13 +1,27 @@
 // may also want to track byte size of hits/misses/evictions in the future
-#[derive(Debug)] // allow for auto-formatter printing
+#[derive(Debug, Clone)] // allow for auto-formatter printing
 pub enum CacheEvent {
-    Hit  { key_hash: u64, tick: usize }, // found the key in cache
-    Miss { key_hash: u64, tick: usize }, // did not locate key in cache
-    Insert { key_hash: u64, size_bytes: usize, tick: usize }, // inserted a new key into cache (may cause eviction if cache is full)
-    Evict  { key_hash: u64, size_bytes: usize, tick: usize }, // evicted a key from cache to free up space for new insertions
+    Hit { 
+        key: u64,
+        tick: u64 
+    }, // found the key in cache
+    Miss { 
+        key: u64,
+        tick: u64
+    }, // did not locate key in cache
+    Insert { 
+        key: u64, 
+        size_bytes: usize, 
+        tick: u64 
+    }, // inserted a new key into cache (may cause eviction if cache is full)
+    Evict { 
+        key: u64, 
+        size_bytes: usize, 
+        tick: u64
+    }, // evicted a key from cache to free up space for new insertions
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct CacheTrace {
     events: Vec<CacheEvent>,
     enabled: bool, // only enabled during testing/debugging
@@ -27,11 +41,15 @@ impl CacheTrace {
         }
     }
 
-    pub fn get_events(&self) -> &[CacheEvent] {
+    pub fn events(&self) -> &[CacheEvent] {
         &self.events
     }
 
     pub fn clear(&mut self) {
         self.events.clear();
+    }
+
+    pub fn is_enabled(&self) -> bool {
+        self.enabled
     }
 }
