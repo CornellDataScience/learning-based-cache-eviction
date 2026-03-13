b897a7148780d0a0a722a450bdae66cee1090be6
smriti-kumar <mailsmritikumar@gmail.com>
2026-03-13 15:30:33 -0400

    implemented replay engine and added cache trace to cache

--- src/core/cache.rs
+++ src/core/cache.rs
@@ -5,6 +5,8 @@ use crate::core::mainmemory::MainMemory;
 use crate::core::metrics::Metrics;
 use crate::core::policy::{CacheKey, Policy};
 use crate::core::time::Clock;
+use crate::core::trace::CacheTrace;
+use crate::core::trace::CacheEvent;
 
 pub struct Cache<P: Policy, const MM_SIZE: usize> {
     pub capacity: usize,
@@ -13,6 +15,7 @@ pub struct Cache<P: Policy, const MM_SIZE: usize> {
     pub metrics: Metrics,
     pub clock: Clock,
     pub main_memory: MainMemory<MM_SIZE>,
+    pub cache_trace: CacheTrace,
 }
 
 impl<P: Policy, const MM_SIZE: usize> Cache<P, MM_SIZE> {
@@ -24,6 +27,7 @@ impl<P: Policy, const MM_SIZE: usize> Cache<P, MM_SIZE> {
             metrics: Metrics::new(),
             clock: Clock::new(),
             main_memory,
+            cache_trace: CacheTrace::new(true), // enabled is always set to true - need to change
         }
     }
 
@@ -35,12 +39,14 @@ impl<P: Policy, const MM_SIZE: usize> Cache<P, MM_SIZE> {
             entry.on_access(tick);
             self.policy.on_hit(key);
             self.metrics.record_hit();
+            self.cache_trace.record_event(CacheEvent::Hit{key, tick});
             return;
         }
 
         // Miss
         self.metrics.record_miss();
         self.policy.on_miss(key);
+        self.cache_trace.record_event(CacheEvent::Miss{key, tick});
 
         if self.capacity == 0 {
             return;
@@ -49,6 +55,10 @@ impl<P: Policy, const MM_SIZE: usize> Cache<P, MM_SIZE> {
         // If at capacity, ask policy who to evict
         if self.store.len() >= self.capacity {
             if let Some(evict_key) = self.policy.victim() {
+                if let Some(entry) = self.store.get(&evict_key) {
+                    let size_bytes = entry.size_in_bytes;
+                    self.cache_trace.record_event(CacheEvent::Evict{key: evict_key, size_bytes, tick});
+                }
                 self.store.remove(&evict_key);
                 self.policy.remove(evict_key);
                 self.metrics.record_eviction();
@@ -60,6 +70,7 @@ impl<P: Policy, const MM_SIZE: usize> Cache<P, MM_SIZE> {
             let entry = Entry::new(obj.key, obj.size_in_bytes, tick);
             self.store.insert(key, entry);
             self.policy.insert(key);
+            self.cache_trace.record_event(CacheEvent::Insert{key, size_bytes: obj.size_in_bytes, tick});
         }
     }
 }
