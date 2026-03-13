9b63944f719eb6e73bc53c2aebcb4f3d1978388c
smriti-kumar <mailsmritikumar@gmail.com>
2026-03-13 15:30:33 -0400

    implemented replay engine and added cache trace to cache

---
+++ src/core/replay_engine.rs
@@ -0,0 +1,49 @@
+use crate::core::cache::Cache;
+use crate::core::trace::Trace;
+use crate::core::policy::Policy;
+use crate::core::metrics::Metrics;
+use std::fmt;
+
+pub struct ReplayResult {
+    pub request_count: u64,
+    pub eviction_count: u64,
+    pub hit_count: u64,
+    pub hit_rate: f64,
+    pub miss_rate: f64,
+}
+
+impl ReplayResult {
+    pub fn from_metrics(metrics: &Metrics) -> Self {
+        Self {
+            request_count: metrics.request_count,
+            eviction_count: metrics.eviction_count,
+            hit_count: metrics.hit_count,
+            hit_rate: metrics.hit_rate(),
+            miss_rate: metrics.miss_rate(),
+        }
+    }
+}
+
+impl fmt::Display for ReplayResult {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Request Count: {}\nHit count: {}\nEviction Count: {}\nHit Rate: {:.3}\nMiss Rate: {:.3}",
+            self.request_count,
+            self.hit_count,
+            self.eviction_count,
+            self.hit_rate,
+            self.miss_rate
+        )
+    }
+}
+
+pub fn replay_trace<P: Policy, const MM_SIZE: usize>(trace: &Trace, cache: &mut Cache<P, MM_SIZE>) -> ReplayResult {
+    // might want to reset cache metrics here but i didn't do that in case we wanted to test a phase shift
+    // but something to keep in mind because cache could have been used before replaying this trace
+    for req in trace.get_requests() {
+        cache.access(req.key);
+    }
+    let results = ReplayResult::from_metrics(&cache.metrics);
+    return results
+}
