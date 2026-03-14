16a6382f92e5e46608b4b075aacf1fc65fb7d65e
Mukund <mg2476@cornell.edu>
2026-03-14 15:29:38 -0400

    - changed cache and main memory to both use entries instead of having separate memory objects, entries for both are paramterized on a single global variable in main memory
    - added an array of bytes to entry so that it can hold data
    
    - changed keys in trace.rs to be cachekeys instead of u64s to be
      consistent with the abstraction
    
    - added a get_requests function that returns a vector of cachekeys. did
      not implement a seperate request struct since i don't think that's
    necessary

--- src/core/mainmemory.rs
+++ src/core/mainmemory.rs
@@ -1,21 +1,13 @@
 use std::collections::HashMap;
 
 use crate::core::policy::CacheKey;
+use crate::core::entry::Entry;
 
-#[derive(Debug, Clone)]
-pub struct MemoryObject {
-    pub key: CacheKey,
-    pub size_in_bytes: usize,
-}
-
-impl MemoryObject {
-    pub fn new(key: CacheKey, size_in_bytes: usize) -> Self {
-        Self { key, size_in_bytes }
-    }
-}
+//define entry size 
+pub const ENTRY_SIZE : usize = 8;
 
 pub struct MainMemory<const SIZE: usize> {
-    pub mem: HashMap<CacheKey, MemoryObject>,
+    pub mem: HashMap<CacheKey, Entry<CacheKey, ENTRY_SIZE>>,
 }
 
 impl<const SIZE: usize> MainMemory<SIZE> {
@@ -25,11 +17,11 @@ impl<const SIZE: usize> MainMemory<SIZE> {
         }
     }
 
-    pub fn fetch(&self, key: &CacheKey) -> Option<&MemoryObject> {
+    pub fn fetch(&self, key: &CacheKey) -> Option<&Entry<CacheKey, ENTRY_SIZE>> {
         self.mem.get(key)
     }
 
-    pub fn insert(&mut self, obj: MemoryObject) {
+    pub fn insert(&mut self, obj: Entry<CacheKey, ENTRY_SIZE>) {
         self.mem.insert(obj.key, obj);
     }
 
